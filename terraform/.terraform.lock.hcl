9d9e75c49d04d85759d1336ea82f09fda0e966d3
Speculor <speculorconsulting@gmail.com>
2021-12-21 10:35:46 -0400

    added dev marketplace

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -4,7 +4,7 @@
 provider "registry.terraform.io/hashicorp/archive" {
   version = "2.2.0"
   hashes = [
-    "h1:CIWi5G6ob7p2wWoThRQbOB8AbmFlCzp7Ka81hR3cVp0=",
+    "h1:62mVchC1L6vOo5QS9uUf52uu0emsMM+LsPQJ1BEaTms=",
     "zh:06bd875932288f235c16e2237142b493c2c2b6aba0e82e8c85068332a8d2a29e",
     "zh:0c681b481372afcaefddacc7ccdf1d3bb3a0c0d4678a526bc8b02d0c331479bc",
     "zh:100fc5b3fc01ea463533d7bbfb01cb7113947a969a4ec12e27f5b2be49884d6c",
@@ -23,7 +23,7 @@ provider "registry.terraform.io/hashicorp/aws" {
   version     = "3.70.0"
   constraints = ">= 2.0.0, >= 2.49.0, >= 3.0.0, >= 3.41.0, ~> 3.70.0"
   hashes = [
-    "h1:E5IKHXzPGGSizZM5rHKzNCzpwQ7lWPXmmJnms82uzDk=",
+    "h1:7rt5BdmiBJOFlNHstjwg2tDP4b+/+4sSvjz5ywP4HJE=",
     "zh:0af710e528e21b930899f0ac295b0ceef8ad7b623dd8f38e92c8ec4bc7af0321",
     "zh:4cabcd4519c0aae474d91ae67a8e3a4a8c39c3945c289a9cf7c1409f64409abe",
     "zh:58da1a436facb4e4f95cd2870d211ed7bcb8cf721a4a61970aa8da191665f2aa",
@@ -42,7 +42,7 @@ provider "registry.terraform.io/hashicorp/local" {
   version     = "2.1.0"
   constraints = ">= 1.2.0"
   hashes = [
-    "h1:EYZdckuGU3n6APs97nS2LxZm3dDtGqyM4qaIvsmac8o=",
+    "h1:PaQTpxHMbZB9XV+c1od1eaUvndQle3ZZHx79hrI6C3k=",
     "zh:0f1ec65101fa35050978d483d6e8916664b7556800348456ff3d09454ac1eae2",
     "zh:36e42ac19f5d68467aacf07e6adcf83c7486f2e5b5f4339e9671f68525fc87ab",
     "zh:6db9db2a1819e77b1642ec3b5e95042b202aee8151a0256d289f2e141bf3ceb3",
@@ -61,7 +61,7 @@ provider "registry.terraform.io/hashicorp/null" {
   version     = "3.1.0"
   constraints = ">= 2.0.0"
   hashes = [
-    "h1:vpC6bgUQoJ0znqIKVFevOdq+YQw42bRq0u+H3nto8nA=",
+    "h1:grYDj8/Lvp1OwME+g1AsECPN1czO5ssSf+8fCluCHQY=",
     "zh:02a1675fd8de126a00460942aaae242e65ca3380b5bb192e8773ef3da9073fd2",
     "zh:53e30545ff8926a8e30ad30648991ca8b93b6fa496272cd23b26763c8ee84515",
     "zh:5f9200bf708913621d0f6514179d89700e9aa3097c77dac730e8ba6e5901d521",
@@ -80,7 +80,7 @@ provider "registry.terraform.io/hashicorp/random" {
   version     = "3.1.0"
   constraints = ">= 2.2.0"
   hashes = [
-    "h1:BZMEPucF+pbu9gsPk0G0BHx7YP04+tKdq2MrRDF1EDM=",
+    "h1:9cCiLO/Cqr6IUvMDSApCkQItooiYNatZpEXmcu0nnng=",
     "zh:2bbb3339f0643b5daa07480ef4397bd23a79963cc364cdfbb4e86354cb7725bc",
     "zh:3cd456047805bf639fbf2c761b1848880ea703a054f76db51852008b11008626",
     "zh:4f251b0eda5bb5e3dc26ea4400dba200018213654b69b4a5f96abee815b4f5ff",
@@ -99,17 +99,7 @@ provider "registry.terraform.io/hashicorp/template" {
   version     = "2.2.0"
   constraints = ">= 2.0.0"
   hashes = [
-    "h1:94qn780bi1qjrbC3uQtjJh3Wkfwd5+tTtJHOb7KTg9w=",
-    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
-    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
-    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
-    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
-    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
-    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
-    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
-    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
-    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
-    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+    "h1:5uwJYleolZhv51FGNSi9Bb5i/ecrlmct2AK0untn4KU=",
   ]
 }
 
@@ -117,7 +107,7 @@ provider "registry.terraform.io/hashicorp/time" {
   version     = "0.7.2"
   constraints = ">= 0.7.0"
   hashes = [
-    "h1:YYLAfhMFP5nhV2iZPslqsLkZN+6sZo7gMJW7pLcLfM8=",
+    "h1:/OoDbNb/z86XjvSlgna568gttFfAk9SoWRa277b3Pgw=",
     "zh:0bbe0158c2a9e3f5be911b7e94477586110c51746bb13d102054f22754565bda",
     "zh:3250af7fd49b8aaf2ccc895588af05197d886e38b727e3ba33bcbb8cc96ad34d",
     "zh:35e4de0437f4fa9c1ad69aaf8136413be2369ea607d78e04bb68dc66a6a520b8",
---
+++ terraform/marketplace.tf
@@ -0,0 +1,175 @@
+# Frontend
+
+module "marketplace_universe_xyz_acm_certificate" {
+  source                      = "cloudposse/acm-request-certificate/aws"
+  version                     = "0.15.1"
+  zone_name                   = "universe.xyz"
+  domain_name                 = "marketplace.dev.universe.xyz"
+  wait_for_certificate_issued = true
+}
+
+module "marketplace_universe_xyz_frontend" {
+  source             = "cloudposse/cloudfront-s3-cdn/aws"
+  version            = "0.79.0"
+  name               = "marketplace.dev.universe.xyz"
+  encryption_enabled = true
+  environment        = "marketplace"
+  # DNS Settings
+  parent_zone_id      = aws_route53_zone.main.zone_id
+  acm_certificate_arn = module.marketplace_universe_xyz_acm_certificate.arn
+  dns_alias_enabled   = true
+  aliases             = ["marketplace.dev.universe.xyz"]
+  ipv6_enabled        = true
+  # Caching Settings
+  default_ttl = 300
+  compress    = true
+  # Website settings
+  website_enabled = true
+  index_document  = "index.html" # absolute path in the S3 bucket
+  error_document  = "index.html" # absolute path in the S3 bucket
+  custom_error_response = [{
+    error_caching_min_ttl = "0"
+    error_code            = "404"
+    response_code         = "200"
+    response_page_path    = "/index.html"
+  }]
+  logging_enabled          = false
+
+  lambda_function_association = [{
+    event_type   = "viewer-request"
+    include_body = false
+    lambda_arn   = module.marketplace_frontend_basic_auth.arn
+  }]
+  minimum_protocol_version = "TLSv1.2_2019"
+  depends_on               = [module.marketplace_universe_xyz_acm_certificate]
+}
+
+
+resource "aws_s3_bucket" "marketplace_lambda" {
+  bucket = "universe-xyz-marketplace-lambda-functions"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
+
+module "marketplace_frontend_basic_auth" {
+  source                 = "transcend-io/lambda-at-edge/aws"
+  version                = "0.4.0"
+  name                   = "marketplace_frontend_basic_auth"
+  description            = "Add basic-auth for marketplace frontend"
+  runtime                = "nodejs12.x"
+  lambda_code_source_dir = "lambda_functions/basic-auth"
+  s3_artifact_bucket     = aws_s3_bucket.marketplace_lambda.id
+
+  tags = {
+    Environment = "marketplace"
+  }
+}
+
+resource "aws_s3_bucket" "universeapp_assets_marketplace" {
+  bucket = "universeapp-assets-marketplace"
+  acl    = "public-read"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["https://marketplace.dev.universe.xyz","https://marketplace-marketplacedevuniversexyz-origin.s3-website-us-east-1.amazonaws.com"]
+    expose_headers  = []
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name        = "universeapp-assets-marketplace"
+    Project     = "kekdao"
+    Environment = "marketplace"
+  }
+}
+resource "aws_s3_bucket_policy" "universeapp_assets_marketplace" {
+  bucket = aws_s3_bucket.universeapp_assets_marketplace.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "universeappAssetsmarketplace"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject","s3:GetObjectVersion"]
+        Resource = [
+          aws_s3_bucket.universeapp_assets_marketplace.arn,
+          "${aws_s3_bucket.universeapp_assets_marketplace.arn}/*",
+        ]
+#        Condition = {
+#          IpAddress = {
+#            "aws:SourceIp" = "0.0.0.0/0"
+#          }
+#        }
+      },
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "universeapp_assets_marketplace" {
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      aws_s3_bucket.universeapp_assets_marketplace.arn
+    ]
+    effect = "Allow"
+  }
+  statement {
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      format("%s/*", aws_s3_bucket.universeapp_assets_marketplace.arn)
+    ]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_user" "universeapp_assets_marketplace" {
+  name = "universeapp-assets-marketplace"
+  path = "/app/"
+
+  tags = {
+    Environment = "marketplace"
+  }
+}
+
+resource "aws_iam_access_key" "universeapp_assets_marketplace" {
+  user = aws_iam_user.universeapp_assets_marketplace.name
+}
+
+resource "aws_iam_user_policy" "universeapp_assets_marketplace" {
+  name   = "universeapp-assets-marketplace"
+  user   = aws_iam_user.universeapp_assets_marketplace.name
+  policy = data.aws_iam_policy_document.universeapp_assets_marketplace.json
+}
+
+output "universeapp_assets_marketplace_access_key_id" {
+  value = aws_iam_access_key.universeapp_assets_marketplace.id
+}
+
+output "universeapp_assets_marketplace_secret_access_key" {
+  sensitive = true
+  value     = aws_iam_access_key.universeapp_assets_marketplace.secret
+}
