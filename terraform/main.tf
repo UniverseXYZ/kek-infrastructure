b8cf2a24aa7480f222393efb7e24e0b38df13461
Speculor <speculorconsulting@gmail.com>
2022-03-09 06:31:37 -0400

    pinning aws version

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
       name = "kek-infrastructure"
     }
   }
+
+  required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 3.70.0"
+     }
+   }
 }
 
 provider "aws" {
