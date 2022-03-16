e67bb1e985040d0852530269b0cb6c651c73ba21
Speculor <speculorconsulting@gmail.com>
2022-03-16 15:09:06 -0400

    testing adding s3 password to cloudfront origin bucket

--- terraform/dev.tf
+++ terraform/dev.tf
@@ -75,9 +75,51 @@ module "dev_universe_xyz_acm_certificate" {
   wait_for_certificate_issued = true
 }
 
+module "lambda_at_edge" {
+  source  = "cloudposse/cloudfront-s3-cdn/aws//modules/lambda@edge"
+  version = "0.82.3"
+  functions = {
+    viewer_request = {
+      source = [{
+        content  = <<-EOT
+        exports.handler = async (event, context, callback) => {
+
+            const request = event.Records[0].cf.request;
+            const headers = request.headers;
+
+            const user = 'graviton';
+            const pass = 'IsAwesome2022';
+
+            const basicAuthentication = 'Basic ' + new Buffer(user + ':' + pass).toString('base64');
+
+            if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != basicAuthentication) {
+                const body = 'You are not authorized to enter';
+                const response = {
+                    status: '401',
+                    statusDescription: 'Unauthorized',
+                    body: body,
+                    headers: {
+                        'www-authenticate': [{key: 'WWW-Authenticate', value:'Basic'}]
+                    },
+                };
+                callback(null, response);
+            }
+            callback(null, request);
+        };
+        EOT
+        filename = "index.js"
+      }]
+      runtime      = "nodejs12.x"
+      handler      = "index.handler"
+      event_type   = "viewer-request"
+      include_body = false
+    }
+  }
+}
+
 module "dev_universe_xyz_frontend" {
   source             = "cloudposse/cloudfront-s3-cdn/aws"
-  version            = "0.40.0"
+  version            = "0.82.3"
   name               = "dev.universe.xyz"
   encryption_enabled = true
   environment        = "dev"
@@ -92,6 +134,11 @@ module "dev_universe_xyz_frontend" {
   compress    = true
   # Website settings
   website_enabled = true
+  s3_website_password_enabled = true
+  allow_ssl_requests_only = false
+  cors_allowed_methods    = ["GET", "HEAD"]
+  cors_allowed_origins    = ["dev.universe.xyz"]
+  cors_allowed_headers    = ["*"]
   index_document  = "index.html" # absolute path in the S3 bucket
   error_document  = "index.html" # absolute path in the S3 bucket
   custom_error_response = [{
@@ -102,11 +149,21 @@ module "dev_universe_xyz_frontend" {
   }]
   logging_enabled = false
 
-  lambda_function_association = [{
-    event_type   = "viewer-request"
-    include_body = false
-    lambda_arn   = module.dev_frontend_basic_auth.arn
-  }]
+  lambda_function_association = module.lambda_at_edge.lambda_function_association
+  geo_restriction_locations = [
+    "BY", # Belarus
+    "CU", # Cuba
+    "IR", # Iran
+    "IQ", # Iraq
+    "CI", # Côte d'Ivoire
+    "LR", # Liberia
+    "KP", # North Korea
+    "SD", # Sudan
+    "SY", # Syria
+    "RU", # Russia
+    "ZW", # Zimbabwe
+  ]
+  geo_restriction_type     = "blacklist"
   minimum_protocol_version = "TLSv1.2_2019"
   depends_on               = [module.dev_universe_xyz_acm_certificate]
 }
